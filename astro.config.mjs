7aff3b4dccfd0998b84a2d51eccc58390cb5593a
theAdhocracy <murrayadcock@gmail.com>
2024-08-19 14:50:05 +0100

    :heavy_plus_sign: Lightning CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from "astro/config";
-
 import netlify from "@astrojs/netlify";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		css: {
+			transformer: "lightningcss",
+		},
+	},
 	output: "server",
 	adapter: netlify(),
 });
