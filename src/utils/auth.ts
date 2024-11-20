a7338b5fbcf9dd7f7f3350bb16c0c807361a0c2b
theAdhocracy <murrayadcock@gmail.com>
2024-11-20 18:39:07 +0000

    :lock: More granular auth visibility

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,33 @@
-export const checkAuth = (cookie: any) => {
-    // Check against validation token
-    const validCode = import.meta.env.AUTH_VALIDATION;
-    const isAuthenticated = cookie && cookie.value === validCode;
+export const checkAuth = (cookie: any, dateToCheck?: string) => {
+	let isAuthenticated = false;
+	let isDateLocked = null;
+	let lockDate = null;
+	let authLevel = 0;
 
-    return isAuthenticated;
-}
+	// Check against validation token
+	const validLevel1 = import.meta.env.AUTH_VALIDATION_1;
+	const validLevel2 = import.meta.env.AUTH_VALIDATION_2;
+
+	if (cookie) {
+		isAuthenticated =
+			cookie.value === validLevel1 || cookie.value === validLevel2;
+	}
+
+	// Determine level of authorisation
+	if (isAuthenticated) {
+		authLevel = cookie.value === validLevel1 ? 1 : 2;
+	}
+
+	// Check date
+	if (dateToCheck) {
+		const today = new Date();
+		lockDate = new Date(today.setMonth(today.getMonth() - 3));
+		isDateLocked = new Date(dateToCheck) > lockDate;
+
+		if (!isDateLocked && !isAuthenticated) {
+			authLevel = 1;
+		}
+	}
+
+	return { isAuthenticated, authLevel, isDateLocked, lockDate };
+};
